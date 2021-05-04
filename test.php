9b22cbb852540e5f736e9d962bfa26e13b1206f6
Suluh Damar G <staf_tidev9@kreditmandiri.co.id>
2021-05-04 15:33:23 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,87 @@
+
+<?php
+    $num = 153;
+    
+    echo narcissistic($num);
+  
+ 
+    function narcissistic($num){
+       $nar = 0;
+       $length = strlen($num);
+
+       for($i = 0; $i < $length; $i++ ){
+            
+            $digit = substr($num, $i, 1);
+            $nar = $nar + (pow($digit,$length));
+       }
+       if($num == $nar){
+           return "true";
+       }else{
+           return "false";
+       }
+   }
+  
+
+?>
+
+<?php
+
+    $arr =  array(2, 4, 0, 100, 4, 11, 2602, 36);
+    echo outlier($arr);
+
+    function outlier($arr){
+        $ganjil = [];
+        $genap  = [];
+        $length = count($arr);
+
+        for($i = 0; $i < $length; $i++){
+            if($arr[$i] < 0){
+                $num = abs($arr[$i]);
+            }else{
+                $num = $arr[$i];
+            }
+    
+            if($num%2 == 1){
+                array_push($ganjil, $arr[$i]);
+            }else{
+                array_push($genap, $arr[$i]);
+            }
+            
+        }
+    
+        if(count($ganjil) == 1){
+            return $ganjil[0];
+        } 
+        else if(count($genap) == 1){
+            return $genap[0];
+        }
+        else{
+            return 'false';
+        }
+
+    }
+
+    
+    
+?>
+
+
+<?php
+    $hay = array("red", "blue", "yellow", "black", "grey");
+    $ned = "grey";
+    echo findNeedle($hay, $ned);
+
+    function findNeedle($hay, $ned){
+        $length = count($hay);
+
+        for($i = 0; $i < $length; $i++){
+            if($hay[$i] == $ned ){
+                return $i;
+            }
+        }
+    }
+
+?>
+
+
+
